813f9ef12e5296b2e68795e2407ddaa65dfc2f57
ponugupati-hema <41770754+ponugupati-hema@users.noreply.github.com>
2018-07-27 12:47:53 +0530

    Create program1.py

---
+++ program1.py
@@ -0,0 +1,8 @@
+#check whether the given number is positive or negative or zero
+num=input("enter the number")
+if(num>0):
+       print("Positive")
+elif(num<0):
+       print("Negative") 
+else:
+       print("Zero")
